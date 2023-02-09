9d3585a0efa81d015b57d86a3b7d99bf26e7ec16
FeverTheo <tete.fever51@gmail.com>
2023-02-09 15:37:01 +0100

    code source nombre aléatoire

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+// Je définis mes variables
+var less = 'cardwl';
+var more = 'cardwm';
+var victory = 'cardwv';
+var value = 0;
+var valuemin = 1;
+var valuemax = 10;
+var random = Math.floor(Math.random()*(valuemax-valuemin))+valuemin;
+console.log(random);
+var entry = document.querySelector('#entry');
+
 function testGo(){
+    console.log(entry.value);
+    // Je donne mes conditions
+    if(entry < random ){
+        less
+    }
+
+    else if(entry > random){
+        more
+    }
     
+    else{
+        victory
+    }
 }
